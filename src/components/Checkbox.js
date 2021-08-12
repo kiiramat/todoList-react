f6c6b37ea4dea78bdeb01896525086f09fa90361
kiiramat <kiiramati@gmail.com>
2021-08-12 19:34:57 +0100

    start of todo list

---
+++ src/components/Checkbox.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Checkbox() {
+    return (
+        <div className="individual-task">
+            <input className="task-checkbox" type="checkbox" />
+            <label className="task-text">Placeholder here</label>
+        </div>
+    )
+}
+
+export default Checkbox;
