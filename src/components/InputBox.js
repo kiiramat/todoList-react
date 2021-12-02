3c13eda2206964b0b3483797fd4e6ada6e56ebc7
kiiramat <kiiramati@gmail.com>
2021-12-02 18:05:54 +0000

    formatted code

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,28 +1,23 @@
 import React from "react";
 
-class InputBox extends React.Component {
-    constructor() {
-      super();
-    }
-  
-    onInputChange = (event) => {
-      const { name, value } = event.target;
-      this.props.handleChange(name, value);
-    }
-  
-    render() {
-      return (
-        <div>
-          <input
-            type="text"
-            name={this.props.name}
-            placeholder={this.props.placeholder}
-            onChange={this.onInputChange}
-            autoComplete="off"
-          />
-        </div>
-      );
-    }
-  }
+const InputBox = function(props) {
+  const onInputChange = (event) => {
+    const { name, value } = event.target;
+    props.handleChange(name, value);
+  };
 
-export default InputBox
+  return (
+    <div>
+      <input
+        type="text"
+        name={props.name}
+        placeholder={props.placeholder}
+        onChange={onInputChange}
+        autoComplete="off"
+      />
+    </div>
+  );
+}
+
+
+export default InputBox;
