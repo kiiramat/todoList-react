fb42c400f7d1bd66a6211075e02796a8f5f3ef1e
kiiramat <kiiramati@gmail.com>
2021-12-11 15:38:33 +0000

    checkbox added

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,29 @@
-import React from "react";
-import "./Task.css"
+import React, { useState } from "react";
+import "./Task.css";
 
 function Task(props) {
+  const [checkedState, setCheckedState] = useState(false);
+
   const onClickEvent = () => {
     props.handleClick(props.task);
   };
 
+  const onCheckboxChange = (event) => {
+    const { checked } = event.target;;
+    setCheckedState(checked);
+  };
+
+
   return (
     <li className="list-item">
       <button className="delete-task-button" onClick={onClickEvent}> x </button>
+      <input
+        className="checkbox"
+        type="checkbox"
+        name={props.task}
+        checked={checkedState}
+        onChange={onCheckboxChange}
+      />
       <span className="task-text" >{props.task}</span>
     </li>
   );
